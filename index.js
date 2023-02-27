1ebea2d3954db74e68471b5534ac8163fe6902f4
Abdullah Azeem <aa6367@sd62.bc.ca>
2023-02-27 11:13:35 -0800

    comments

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const BG_COLOUR = '#231f20';
 const SNAKE_COLOUR = '#c2c2c2';
 const FOOD_COLOUR = '#e66916';
 
+//Listening to specific server
 const socket = io('https://socketserver-kr9u.onrender.com');
 
+// Getting socket trigger events
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
 socket.on('gameOver', handleGameOver);
@@ -11,6 +13,7 @@ socket.on('gameCode', handleGameCode);
 socket.on('unknownCode', handleUnknownCode);
 socket.on('tooManyPlayers', handleTooManyPlayers);
 
+//Gets html values from index.html
 const gameScreen = document.getElementById('gameScreen');
 const initialScreen = document.getElementById('initialScreen');
 const newGameBtn = document.getElementById('newGameButton');
@@ -18,15 +21,17 @@ const joinGameBtn = document.getElementById('joinGameButton');
 const gameCodeInput = document.getElementById('gameCodeInput');
 const gameCodeDisplay = document.getElementById('gameCodeDisplay');
 
+//Click even for game join or new game
 newGameBtn.addEventListener('click', newGame);
 joinGameBtn.addEventListener('click', joinGame);
 
-
+//Initilizing new game
 function newGame() {
   socket.emit('newGame');
   init();
 }
 
+//Joining game
 function joinGame() {
   const code = gameCodeInput.value;
   socket.emit('joinGame', code);
@@ -37,6 +42,7 @@ let canvas, ctx;
 let playerNumber;
 let gameActive = false;
 
+//Initilizing game screen
 function init() {
   initialScreen.style.display = "none";
   gameScreen.style.display = "block";
@@ -46,6 +52,7 @@ function init() {
 
   canvas.width = canvas.height = 600;
 
+  //Setting game canvas
   ctx.fillStyle = BG_COLOUR;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -53,10 +60,12 @@ function init() {
   gameActive = true;
 }
 
+//When key pressed
 function keydown(e) {
   socket.emit('keydown', e.keyCode);
 }
 
+//Painting game every framce
 function paintGame(state) {
   ctx.fillStyle = BG_COLOUR;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -68,6 +77,7 @@ function paintGame(state) {
   ctx.fillStyle = FOOD_COLOUR;
   ctx.fillRect(food.x * size, food.y * size, size, size);
 
+  //Painting snakes
   paintPlayer(state.players[0], size, SNAKE_COLOUR);
   paintPlayer(state.players[1], size, 'red');
 }
@@ -75,16 +85,19 @@ function paintGame(state) {
 function paintPlayer(playerState, size, colour) {
   const snake = playerState.snake;
 
+  //painting snake with specifc position
   ctx.fillStyle = colour;
   for (let cell of snake) {
     ctx.fillRect(cell.x * size, cell.y * size, size, size);
   }
 }
 
+//Setting player to specific num 1 or 2
 function handleInit(number) {
   playerNumber = number;
 }
 
+// Request for new frame from paitnt game every frame
 function handleGameState(gameState) {
   if (!gameActive) {
     return;
@@ -93,14 +106,17 @@ function handleGameState(gameState) {
   requestAnimationFrame(() => paintGame(gameState));
 }
 
+//What happens when game is over
 function handleGameOver(data) {
   if (!gameActive) {
     return;
   }
   data = JSON.parse(data);
 
+  //deactivates game
   gameActive = false;
 
+  //Gives each player their respective game ove prompt
   if (data.winner === playerNumber) {
     alert('You Win!');
   } else {
@@ -108,20 +124,25 @@ function handleGameOver(data) {
   }
 }
 
+//Sets room name code to specific code
 function handleGameCode(gameCode) {
   gameCodeDisplay.innerText = gameCode;
 }
 
+//Alerts user that game code is invalid
 function handleUnknownCode() {
   reset();
   alert('Unknown Game Code')
 }
 
+
+//Alerts user that game is full
 function handleTooManyPlayers() {
   reset();
   alert('This game is already in progress');
 }
 
+//Resets entire game
 function reset() {
   playerNumber = null;
   gameCodeInput.value = '';
